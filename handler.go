27c3dcf6c680687f656aeb4e6ed59d74114b89a6
Tobias Schäfer <github@blackox.org>
2025-11-29 16:43:40 +0100

    chore: remove license header; interfere with go do

--- handler.go
+++ handler.go
@@ -1,7 +1,3 @@
-/*
-Copyright (c) 2025 Tobias Schäfer. All rights reserved.
-Licensed under the MIT License, see LICENSE file in the project root for details.
-*/
 package slogjournal
 
 import (
--- journal.go
+++ journal.go
@@ -1,5 +1,1 @@
-/*
-Copyright (c) 2025 Tobias Schäfer. All rights reserved.
-Licensed under the MIT License, see LICENSE file in the project root for details.
-*/
 package slogjournal
